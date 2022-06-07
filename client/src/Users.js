d2586ef4809430bde0657140455c3a299f7a0e26
Coder yard <wskyard96@gmail.com>
2022-06-07 18:32:42 +0900

    FEAT: Render data using express api

---
+++ client/src/Users.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function UsersInfo({ color, name, isSpecial }) {
+  return (
+    <div style={{ color }}>
+      
+    </div>
+  );
+}
+
+Hello.defaultProps = {
+  name: '이름없음'
+}
+
+export default UsersInfo;
